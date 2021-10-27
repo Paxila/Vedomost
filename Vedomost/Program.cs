132e44e4a37c07f1a34a6de663ef9807737eabec
Pavel <paxila.1.130@gmail.com>
2021-10-27 16:04:59 +0700

    Добавьте файлы проекта.

---
+++ Vedomost/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Vedomost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Name = new List<string> { }; 
+            List<string> Surname = new List<string> { }; 
+            List<string> Price = new List<string> { }; 
+            List<string> Date = new List<string> { }; 
+            string stroka = null;
+            string SumSmbl = null; 
+            int spases = 0; 
+            int maxLongDate = 0; 
+        
+
+            int sum = 0;
+            int maxSum = 0; 
+            int index = 0; 
+            try
+            {
+                
+                using (StreamReader sr = new StreamReader("Vedomost.txt"))
+                {
+                    do 
+                    {
+                        stroka = sr.ReadLine(); 
+                        spases = 0; 
+                        if (stroka == null) break; 
+                        foreach (var simvol in stroka)
+                        {
+                            if (simvol == ' ')
+                            {
+                                switch (spases)
+                                {
+                                    case 0: 
+                                        Name.Add(SumSmbl);
+                                        break;
+                                    case 1:
+                                        Surname.Add(SumSmbl);
+                                        break;
+                                    case 2:
+                                        Price.Add(SumSmbl);
+                                        break;
+                                }
+                                spases++; 
+                                SumSmbl = null;
+                                continue;
+                            }
+                            SumSmbl += simvol;
+                        }
+                        Date.Add(SumSmbl);
+                        SumSmbl = null;
+                    } while (stroka != null); //конец строки
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            int CountLines = Name.Count;
+            ////////////////////////////////////////////////////////////////////////////
+            for (int i = 0; i < CountLines; i++) //Вывод таблицы
+            {
+                Console.Write( Name[i] + " " + Surname[i]);
+                for (int j = Name[i].Length + Surname[i].Length + 1; j <= 20; j++) { Console.Write(" "); }
+
+                Console.Write(Price[i] + "$");
+
+                for (int j = Price[i].Length; j <= 15; j++) { Console.Write(" "); }
+                Console.WriteLine(Date[i]);
+            }
+            Console.WriteLine();
+            ////////////////////////////////////////////////////////////////////////////    
+            for (int i = 0; i < CountLines; i++) //Расчет максимальной и общей суммы
+            {
+                sum += Convert.ToInt32(Price[i]);//макс
+                if (maxLongDate < Date[i].Length) maxLongDate = Date[i].Length;
+
+                if (Convert.ToInt32(Price[i]) > maxSum) //общ
+                {
+                    maxSum = Convert.ToInt32(Price[i]);
+                    index = i; 
+                }
+            }
+            ////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Всего выплаченно: " + sum +"$");
+            Console.WriteLine("Максимальная выплаченная сумма: " + Name[index] + " " + Surname[index] + " = " + maxSum + "$");
+
+            Console.ReadKey();
+        }
+    }
+}
+
